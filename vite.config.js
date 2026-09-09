3d54b43fa494ee6d4b9429fad64a50170c879a2d
phoomth1407 <112713824+phoomth1407@users.noreply.github.com>
2026-09-10 00:31:23 +0700

    Fix Vite path alias for production build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import path from "node:path"
+import { fileURLToPath } from "node:url"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
