5468dc70cfda73256687203dbb5b64c5b5a096c2
phoomth1407 <112713824+phoomth1407@users.noreply.github.com>
2026-09-14 22:29:53 +0700

    Fix Vitest globals in scoring tests

--- tests/assessmentScoring.test.js
+++ tests/assessmentScoring.test.js
@@ -1,3 +1,4 @@
+import { describe, expect, it } from "vitest";
 import { assessmentCategories } from "../src/lib/assessmentQuestions";
 import { computeAssessmentResult } from "../src/lib/assessmentScoring";
 
