4286e0e357a962d6003a69eb148fc35781e89e41
phoomth1407 <112713824+phoomth1407@users.noreply.github.com>
2026-09-19 19:16:11 +0700

    ui: add fx component index.js

---
+++ src/components/fx/index.js
@@ -0,0 +1,4 @@
+export { default as BreathingBackdrop } from './BreathingBackdrop';
+export { default as Surface } from './Surface';
+export { default as PageTransition } from './PageTransition';
+export { Reveal, Stagger, StaggerItem } from './Reveal';
